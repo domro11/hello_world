7b8cf266863ca2301baac74252cc73b7cea138aa
Your Name~domro11 <dominik.roser@icloud.com>
2022-09-03 22:25:27 +0200

    First commit

---
+++ hello_class_package/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='helloworld',
+      version='0.1',  # Development release
+      description='Python Packaging simple Class to say Hello World!',
+      url='https://github.com/manoelgadi/helloworld',
+      author='Prof. Manoel Gadi',
+      author_email='mfalonso@faculty.ie.edu',
+      license='MIT',
+          packages=['helloworld'],
+      zip_safe=False)
